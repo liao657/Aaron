5670895544d56d72e48b2abc9a3c2743982d97c3
liao657 <w5862591@gmail.com>
2021-03-08 20:31:21 +0800

    說明

--- Aaron.js
+++ Aaron.js
@@ -32,3 +32,29 @@ var scroller = $(window).smoothDivScroll({
 setTimeout(function(){ 
     scroller.smoothDivScroll("startAutoScrolling"); 
 },2000) //2000 is delay in milliseconds 
+
+
+
+
+$("a").each(function (index, element) {
+  console.log("編號:" + index);
+  
+  var target= $(this).attr("data-st-target");
+  var duration= $(this).attr("data-st-dutation");
+
+  console.log("目標：" + target);
+  console.log("時間：" + duration);
+  var offset = $("#"+ target).offset();
+  var top = offset.top;
+
+ console.log("上方:" + top)
+  
+
+ $(this).click(function (e) { 
+     e.preventDefault();
+     
+     $("html").animate({
+         scrollTop:top
+     }, parseInt(duration))
+ });
+});
